981374297f7426efb4af6b4dbec98ce95d4a622d
Anjali Silva <a.silva@utoronto.ca>
2024-02-14 13:30:34 -0500

    Add sample size and median to violin plot

--- R/volumeStats.R
+++ R/volumeStats.R
@@ -297,6 +297,7 @@ visVolumeData <- function(dataARL, institute, years = NA) {
                         y = `Volumes held`,
                         width = .75)) +
     ggplot2::geom_violin() +
+    ggplot2::stat_summary(fun = median, geom = "point", size = 2, color = setColorPalette()[1]) +
     ggplot2::scale_color_manual(values = c(setColorPalette())) +
     ggplot2::labs(y = "Volumes Held",
                   x = "Year",
@@ -306,7 +307,8 @@ visVolumeData <- function(dataARL, institute, years = NA) {
                    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, color = 'black', size = 15),
                    axis.text.y = element_text(color = 'black', size = 15)) +
     ggplot2::scale_y_continuous(labels = scales::label_comma(),
-                                breaks = scales::pretty_breaks(n = 5))
+                                breaks = scales::pretty_breaks(n = 5)) +
+    EnvStats::stat_n_text()
 
 
   return(list(volumeUserInstitute = volumeUserInstitute,
