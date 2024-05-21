d80c853a4ca3b301d20d90fd8945dd542eec43d4
Anjali Silva <a.silva@utoronto.ca>
2024-05-21 17:23:02 -0400

    Initiated tests

---
+++ tests/testthat/test-visTotalLibraryExp.R
@@ -0,0 +1,55 @@
+context("Checking for Plots to Compare Total Library Expenditures Over Years")
+library(libraryStatistics)
+
+test_that("Data generation is as expected", {
+
+  # Generating simulated data
+  trueMu1 <- c(6.5, 6, 6, 6, 6, 6)
+  trueMu2 <- c(2, 2.5, 2, 2, 2, 2)
+
+  trueSigma1 <- diag(6) * 2
+  trueSigma2 <- diag(6)
+
+  <- visTotalLibraryExp(dataARL = ARLDataDownload,
+                      members = c("BOSTON", "TORONTO", "OTTAWA", "LAVAL", "HARVARD"),
+                      years = c(2015, 2016, 2017, 2022, 2018, 2019))
+
+
+  expect_type(simulatedCounts, "list")
+  expect_length(simulatedCounts, 9)
+  expect_s3_class(simulatedCounts, "mplnDataGenerator")
+  expect_identical(trunc(simulatedCounts$observations), 50)
+  expect_identical(trunc(simulatedCounts$dimensionality), 6)
+  expect_named(simulatedCounts, c("dataset", "trueMembership",
+                                  "probaPost", "trueNormFactors",
+                                  "observations", "dimensionality",
+                                  "mixingProportions", "trueMu",
+                                  "trueSigma"))
+  expect_output(str(simulatedCounts), "List of 9")
+  expect_vector(simulatedCounts$mixingProportions, ptype = double(), size = 2)
+})
+
+
+context("Checking for invalid user input")
+test_that("Data generate error upon invalid user input", {
+
+  # nObservations provided as character
+  expect_error(mplnDataGenerator(nObservations = "50",
+                                 dimensionality = 6,
+                                 mixingProportions = c(0.79, 0.21),
+                                 mu = rbind(trueMu1, trueMu2),
+                                 sigma = rbind(trueSigma1, trueSigma2),
+                                 produceImage = "No"))
+
+
+  # Generating simulated data - mu has incorrect dimension
+  trueMu1 <- c(6.5, 6, 6, 6, 6)
+  trueMu2 <- c(2, 2.5, 2, 2, 2)
+
+  expect_error(mplnDataGenerator(nObservations = 50,
+                                 dimensionality = 6,
+                                 mixingProportions = c(0.79, 0.21),
+                                 mu = rbind(trueMu1, trueMu2),
+                                 sigma = rbind(trueSigma1, trueSigma2),
+                                 produceImage = "No"))
+
