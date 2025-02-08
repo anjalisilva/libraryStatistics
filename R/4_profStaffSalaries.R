8668a91a3e9bb9e5a643d1f6bffd219e92357aa4
Anjali Silva <a.silva@utoronto.ca>
2025-02-07 20:10:44 -0500

    Move global variables

--- R/4_profStaffSalaries.R
+++ R/4_profStaffSalaries.R
@@ -135,15 +135,6 @@
 #' @import dplyr
 visProfStaffSalaries <- function(dataARL, members, years = NA) {
 
-  # define objects globally
-  utils::globalVariables(c("Year", "Institution Name", "Total teaching faculty", ".",
-                           "Professional salaries & wages", "proPerFaculty", "expPerFaculty",
-                           "Total fulltime students", "Part-time students, undergraduate and graduate",
-                           "allStudents", "proPerStudent", "expPerStudent", "Part-time graduate students",
-                           "Total fulltime graduate students", "allGradStudents", "profPerGradStudent",
-                           "expPerGradStudent", "totalUndergradStudents", "expPerUndergradStudent",
-                           "Doctor's degrees awarded", "proPerDoctoral", "expPerDoctoral",
-                           "proPerGradStudent", "allgradStudents"))
 
   # Running data for prescreening and adjustmnent with helper functions
   selectedData <- dataAdjustment(dataARL = dataARL)
