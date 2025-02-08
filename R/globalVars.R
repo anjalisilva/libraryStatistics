36735ecd855159922be41e90b9460de926b95fc2
Anjali Silva <a.silva@utoronto.ca>
2025-02-07 20:11:10 -0500

    Added script for global variables

---
+++ R/globalVars.R
@@ -0,0 +1,10 @@
+
+# define objects globally
+utils::globalVariables(c("Year", "Institution Name", "Total teaching faculty", ".",
+                         "Professional salaries & wages", "proPerFaculty", "expPerFaculty",
+                         "Total fulltime students", "Part-time students, undergraduate and graduate",
+                         "allStudents", "proPerStudent", "expPerStudent", "Part-time graduate students",
+                         "Total fulltime graduate students", "allGradStudents", "profPerGradStudent",
+                         "expPerGradStudent", "totalUndergradStudents", "expPerUndergradStudent",
+                         "Doctor's degrees awarded", "proPerDoctoral", "expPerDoctoral",
+                         "proPerGradStudent", "allgradStudents"))
